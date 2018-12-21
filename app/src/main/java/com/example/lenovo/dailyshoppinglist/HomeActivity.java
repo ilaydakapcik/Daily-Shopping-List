bb77c96dcb08487a7724254a90abc9cc2c940a1c
FEYZA\Feyzaa <feyza.erdogan@ogr.sakarya.edu.tr>
2018-12-22 00:30:33 +0300

    Toolbar tasarımı eklendi.

--- app/src/main/java/com/example/lenovo/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/lenovo/dailyshoppinglist/HomeActivity.java
@@ -2,12 +2,20 @@ package com.example.lenovo.dailyshoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar=findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Günlük Alışveriş Listesi");
     }
 }
